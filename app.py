a8658094062d04785205627b67b71b2f3eaf0396
AndreyDOne <andreyjade.delarosa@lspu.edu.ph>
2025-12-05 00:23:26 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -188,4 +188,4 @@ def dashboard():
 if __name__ == '__main__':
     with app.app_context():  
         db.create_all()  # database tables
-    app.run(debug=True)
+    app.run(host-'0.0.0.0'debug=True)
