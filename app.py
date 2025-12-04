be8f974e4f2fb92361944f5885588ece69bcba90
AndreyDOne <andreyjade.delarosa@lspu.edu.ph>
2025-12-05 00:33:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -188,4 +188,5 @@ def dashboard():
 if __name__ == '__main__':
     with app.app_context():  
         db.create_all()  # database tables
-    app.run(host-'0.0.0.0'debug=True)
+
+    app.run(host-'0.0.0.0', debug=True)
