59932eea27c7ccf168fe24e75898f1f36fb6832d
AndreyDOne <andreyjade.delarosa@lspu.edu.ph>
2025-12-05 00:41:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -189,4 +189,5 @@ if __name__ == '__main__':
     with app.app_context():  
         db.create_all()  # database tables
 
-    app.run(host-'0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
